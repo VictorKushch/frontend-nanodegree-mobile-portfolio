608050df7d56e4a914ee853b96243717bc1693d8
Viktar Kushch <viktarkushch@Viktars-MacBook-Pro.local>
2016-11-20 13:34:42 -0500

    Comments in main.js

--- views/js/main.js
+++ views/js/main.js
@@ -449,6 +449,7 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    //requesting and calculating dx and newwidth out of layout affecting loop
     var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[0], size);
     var newwidth = (document.querySelectorAll(".randomPizzaContainer")[0].offsetWidth + dx) + 'px';
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
